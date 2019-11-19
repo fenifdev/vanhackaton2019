6c13494400d3efd883630c195c612ff11de9e97e
Federico Nicolás Flores <flores.federico.nicolas@gmail.com>
2019-11-19 20:52:26 -0300

    REFACTORING: improve design in component event

--- components/Event.js
+++ components/Event.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 const Event = ({event}) => {
 	return (
 		<View>
-	      <View style={{height: 150}}>
-	        <Image source={require('../assets/images/event-hero.png')} style={{width: '100%', height: '100%'}}/>
-	        <Image source={event.countryImage} style={{position: 'absolute', right:10, bottom: -15, marginRight:10}}/>
+	      <View style={styles.heroImageContainer}>
+	        <Image source={require('../assets/images/event-hero.png')} style={styles.heroImage}/>
+	        <Image source={event.countryImage} style={styles.countryImage}/>
 	      </View>
-	      <View style={{padding:10}}>
-	        <Text>{event.title}</Text>
-	        <Text>{event.date}</Text>
-	        <Text>{event.location}</Text>
-	        <Text>{event.deadline}</Text>
+	      <View style={styles.eventContainer}>
+	        <Text style={styles.title}>{event.title}</Text>
+	        <Text style={{color:'#ccc'}}>{event.date}</Text>
+	        <Text style={{color:'#ccc'}}>{event.location}</Text>
+	        <View style={{flexDirection:'row'}}>
+	        	<Text>Deadline:</Text><Text style={{color:'#ccc'}}>{event.deadline}</Text>
+	        </View>
 	      </View>
 	    </View>
 	)
 }
+
+const styles = StyleSheet.create({
+	heroImageContainer: {
+		height: 150
+	},
+	heroImage: {
+		width: '100%', 
+		height: '100%'
+	},
+	countryImage: {
+		position: 'absolute',
+		right:10, 
+		bottom: -15, 
+		marginRight:10
+	},
+	eventContainer: {
+		padding:10
+	},
+	title: {
+		fontSize: 20
+	}
+});
 
 export default Event;
