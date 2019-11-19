2256da5016c2afa3cfaf5f0102c364a27ab221d8
Federico Nicolás Flores <flores.federico.nicolas@gmail.com>
2019-11-19 07:50:46 -0300

    ADD: Basic navigation flow

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function LoginScreen() {
-  return (
+const loginscreen = class LoginScreen extends React.Component {
+  render() {
+    const navigation = this.props.navigation;
+    return (
     <View>
       <Text>Logo Vanhack</Text>
       <TextInput 
-      	autoComplete="off"
-      	autoCapitalize="none"
+        autoComplete="off"
+        autoCapitalize="none"
         autoCorrect={false}
         placeholder="Username"
       />
       <TextInput 
-      	autoComplete="off"
-      	secureTextEntry={true}
-      	autoCapitalize="none"
+        autoComplete="off"
+        secureTextEntry={true}
+        autoCapitalize="none"
         autoCorrect={false}
         placeholder="Password"
       />
       <Text>Forgot Password?</Text>
-      <Button title="Login"></Button>
-      <Text>Register</Text>
+      <Button title="Login" onPress={()=>navigation.navigate('App')}></Button>
+      <Button title="Register" onPress={()=>navigation.navigate('Register')}></Button>
     </View>
   );
+  }
 }
+
+export default loginscreen;
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,38 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, InputText } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function RegisterScreen() {
-  return (
-    <View>
-      <Text>Logo VanHack</Text>
-      <TextInput 
-      	autoComplete="off"
-      	autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Username"
-      />
-      <TextInput 
-      	autoComplete="off"
-      	autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Email"
-      />
-      <TextInput 
-      	autoComplete="off"
-      	secureTextEntry={true}
-      	autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Password"
-      />
-      <TextInput 
-      	autoComplete="off"
-      	secureTextEntry={true}
-      	autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Re-Password"
-      />
-      <Button title="Register"></Button>
-      <Text>Login</Text>
-    </View>
-  );
+const registerscreen = class RegisterScreen extends React.Component {
+  render(){
+    const navigation = this.props.navigation;
+
+    return (
+      <View>
+        <Text>Logo VanHack</Text>
+        <TextInput 
+          autoComplete="off"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Username"
+        />
+        <TextInput 
+          autoComplete="off"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email"
+        />
+        <TextInput 
+          autoComplete="off"
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+        />
+        <TextInput 
+          autoComplete="off"
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Re-Password"
+        />
+        <Button title="Register" onPress={()=>navigation.navigate('App')}></Button>
+        <Button title="Login" onPress={()=>navigation.navigate('Login')}></Button>
+      </View>
+    );
+  }
 }
+
+export default registerscreen;
