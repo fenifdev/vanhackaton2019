0ebad012f76006aa3bc4535cf3d540de406375cc
Federico Nicolás Flores <flores.federico.nicolas@gmail.com>
2019-11-19 13:18:07 -0300

    ADD: logo in login and register screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,36 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 const loginscreen = class LoginScreen extends React.Component {
   render() {
     const navigation = this.props.navigation;
     return (
-    <View>
-      <Text>Logo Vanhack</Text>
-      <TextInput 
-        style={styles.inputText}
-        autoComplete="off"
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Username"
-      />
-      <TextInput 
-        style={styles.inputText}
-        autoComplete="off"
-        secureTextEntry={true}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Password"
-      />
-      <Text>Forgot Password?</Text>
-      <Button style={styles.mainButton} title="Login" onPress={()=>navigation.navigate('App')}></Button>
-      <Button title="Register" onPress={()=>navigation.navigate('Register')}></Button>
+    <View style={styles.viewContainer}>
+      <View style={{marginTop:50,marginBottom:50}}>
+        <Image style={styles.logo} source={require('../assets/images/logo.png')}/>
+      </View>
+      <View style={{flex:1,marginTop:50, width: '100%'}}>
+        <TextInput 
+          style={styles.inputText}
+          autoComplete="off"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Username"
+        />  
+        <TextInput 
+          style={styles.inputText}
+          autoComplete="off"
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+        />
+        <Text>Forgot Password?</Text>
+        <Button style={styles.mainButton} title="Login" onPress={()=>navigation.navigate('App')}></Button>
+        
+      </View>
+      <View style={{padding:10}}>
+        <Button title="Register" onPress={()=>navigation.navigate('Register')}></Button>
+      </View>
     </View>
   );
   }
 }
 
 const styles = StyleSheet.create({
+  viewContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'column'
+  },
+  logo: {
+    height: 40,
+    width: 200,
+  },
   mainButton: {
     backgroundColor: '#0675ce'
   },
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,51 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 const registerscreen = class RegisterScreen extends React.Component {
   render(){
     const navigation = this.props.navigation;
 
     return (
-      <View>
-        <Text>Logo VanHack</Text>
-        <TextInput 
-          style={styles.inputText}
-          autoComplete="off"
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholder="Username"
-        />
-        <TextInput 
-          style={styles.inputText}
-          autoComplete="off"
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholder="Email"
-        />
-        <TextInput 
-          style={styles.inputText}
-          autoComplete="off"
-          secureTextEntry={true}
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholder="Password"
-        />
-        <TextInput 
-          style={styles.inputText}
-          autoComplete="off"
-          secureTextEntry={true}
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholder="Re-Password"
-        />
-        <Button title="Register" onPress={()=>navigation.navigate('App')}></Button>
-        <Button title="Login" onPress={()=>navigation.navigate('Login')}></Button>
+      <View style={styles.viewContainer}>
+        <View style={{marginTop:20,marginBottom:50}}>
+          <Image style={styles.logo} source={require('../assets/images/logo.png')}/>
+        </View>
+        <View style={{flex:1,width:'100%'}}>
+          <TextInput 
+            style={styles.inputText}
+            autoComplete="off"
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Username"
+          />
+          <TextInput 
+            style={styles.inputText}
+            autoComplete="off"
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Email"
+          />
+          <TextInput 
+            style={styles.inputText}
+            autoComplete="off"
+            secureTextEntry={true}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Password"
+          />
+          <TextInput 
+            style={styles.inputText}
+            autoComplete="off"
+            secureTextEntry={true}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Re-Password"
+          />
+          <Button title="Register" onPress={()=>navigation.navigate('App')}></Button>
+        </View>
+        <View style={{padding:10}}>
+          <Button title="Login" onPress={()=>navigation.navigate('Login')}></Button>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  viewContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'column'
+  },
+  logo: {
+    height: 40,
+    width: 200,
+  },
   mainButton: {
     backgroundColor: '#0675ce'
   },
