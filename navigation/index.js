7eca2f3e73c5f32efcab1732c55d2d7db5c4cf6e
Federico Nicolás Flores <flores.federico.nicolas@gmail.com>
2019-11-20 12:23:30 -0300

    ADD: Icon hamburguer menu in the header

--- navigation/index.js
+++ navigation/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -9,12 +9,16 @@ import JobsScreen from '../screens/JobsScreen';
 import EventsScreen from '../screens/EventsScreen';
 import CoursesScreen from '../screens/CoursesScreen';
 import CourseScreen from '../screens/CourseScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const JobsStackNavigator = createStackNavigator({
   Jobs: { 
   	screen: JobsScreen,
   	navigationOptions: ({navigation}) => ({
-      headerLeft: (<Image style={{marginLeft:10}} source={require('../assets/images/logo.png')}/>),
+      headerLeft: (<View style={{flexDirection:'row', paddingLeft:10}}>
+        <Ionicons name="md-menu" size={30} color="#46c1f1" />
+        <Image style={{marginLeft:10}} source={require('../assets/images/logo.png')}/>
+        </View>),
 	})
   }
 });
@@ -23,7 +27,10 @@ const EventsStackNavigator = createStackNavigator({
   Events: { 
   	screen: EventsScreen,
   	navigationOptions: ({navigation}) => ({
-      headerLeft: (<Image style={{marginLeft:10}} source={require('../assets/images/logo.png')}/>),
+      headerLeft: (<View style={{flexDirection:'row', paddingLeft:10}}>
+        <Ionicons name="md-menu" size={30} color="#46c1f1" />
+        <Image style={{marginLeft:10}} source={require('../assets/images/logo.png')}/>
+        </View>),
 	})
   }
 });
@@ -32,7 +39,10 @@ const CoursesStackNavigator = createStackNavigator({
   Courses: { 
   	screen: CoursesScreen,
   	navigationOptions: ({navigation}) => ({
-      headerLeft: (<Image style={{marginLeft:10}} source={require('../assets/images/logo.png')}/>),
+      headerLeft: (<View style={{flexDirection:'row', paddingLeft:10}}>
+        <Ionicons name="md-menu" size={30} color="#46c1f1" />
+        <Image style={{marginLeft:10}} source={require('../assets/images/logo.png')}/>
+        </View>),
 	})
   },
   Course: {
